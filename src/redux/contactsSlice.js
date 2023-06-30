3c4779c353e621b890aba6f8673fbe840cbac375
Okazaki92 <vincentslominski@gmail.com>
2023-06-30 22:14:16 +0200

    fix/ store contacts to localStorage

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,13 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 let storageList =
-  localStorage.getItem("contacts") === undefined
+  localStorage.getItem("contacts") !== undefined
     ? JSON.parse(localStorage.getItem("contacts"))
-    : [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ];
+    : [];
 export const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
@@ -26,7 +21,7 @@ export const contactsSlice = createSlice({
     findContact(state, action) {
       state.filter = action.payload;
     },
-    storeContactsToLocalStorage: (state) => {
+    storeContactsToLocalStorage: (state, action) => {
       localStorage.setItem("contacts", JSON.stringify(state.list));
     },
   },
